05c86e579bd7c675bb3e855d9ed12b7de811d2cd
Ole MORTENSEN <ole.mortensen@me.com>
2018-11-22 00:54:02 +0100

    Add clear button and use lambdas

--- js/convert.js
+++ js/convert.js
@@ -2,25 +2,42 @@ $(document).ready(() => {
 
     $('#submit-button').click((e) => {
 
+    // erase old value
     $('#json-entry').val('');
 
+    // get json as string from field
     var str = $('#java-entry').val();
 
     try {
+	// parse user input as json
         var json = JSON.parse(str);
+	
         var java = '';
 
+        // iterate over all json fields
 	Object.entries(json).forEach(
-        ([k, v]) => {
-            var quote = (v.constructor === String) ? '"' : '';
-            java += 'request("' + k + '", ' + quote + v + quote + ');\n'}
-        );;
+            ([k, v]) => {
+		// use quote around type string but no quote for number or boolean
+                var quote = (v.constructor === String) ? '"' : '';
 
+		// add new line of java code
+                java += `request("${k}",  ${quote}${v}${quote});\n`;
+	    }
+        );
+
+        // display generated java code
         $('#json-entry').val(java);
     
     } catch (error) {
+	// in case of errors, display error in the output field
         $('#json-entry').val(error);
     }
 
    });
+
+
+    $('#clear-button').click((e) => {
+        $('#json-entry').val('');
+        $('#java-entry').val('');
+    });
 });
